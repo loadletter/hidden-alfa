04c6737168b525dd68ba380fd779a752752f9a94
loadletter <loadletter@users.noreply.github.com>
2015-11-09 21:54:51 +0100

    Remove harmful stuff

--- hidden-alfa.py
+++ hidden-alfa.py
@@ -1,39 +1,42 @@
 from StringIO import StringIO
 from PIL import Image
 import zlib
-import tarfile
 import struct
-import os
 
+ZLIB_DECOMPRESS_MAXSIZE=1024*1024*100
 
-class ZlibFormat:
-	def create(self, **kwargs):
-		return zlib.compress(kwargs['data'], level=9)
-	def extract(self, **kwargs):
-		return zlib.extract(kwargs['data'], data)
-		
-class TarFormat:
-	def create(self, **kwargs):
-		fo = StringIO()
-		with tarfile.open(mode='w', fileobj=fo) as tar:
-			tar.add(kwargs['path'], recursive=True)
-		data = fo.getvalue()
-		fo.close()
-		return data
-		
-	def extract(self, **kwargs):
-		#todo
-		return
+class FormatException(Exception):
+	pass
 
-
-def pack_size(self, i):
-	data = struct.pack('<I', i)
-	assert len(data) == 4
-	return data
+def crc32_create(self, data):
+	return struct.pack('<i', zlib.crc32(data)) + data
+def crc32_extract(self, data):
+	crc32 = struct.unpack('<i', data[0:4])
+	if crc32 != zlib.crc32(data[4:]):
+		raise FormatException("CRC32 mismatch")
+	return data[4:]
 	
-def unpack_size(self, data):
-	assert len(data) == 4
-	return struct.unpack('<I', data)
+def zlib_create(self, data):
+	return zlib.compress(data, level=9)
+def zlib_extract(self, data):
+	dec = zlib.decompressobj()
+	try:
+		data = dec.decompress(data, ZLIB_DECOMPRESS_MAXSIZE)
+	except zlib.error as e:
+		raise FormatException("Zlib error %s" % e)
+	if dec.unconsumed_tail:
+		raise FormatException("Zlib decompressed size exceeds %i bytes limit" % ZLIB_DECOMPRESS_MAXSIZE)
+	return (data, dec.unused_data)
+
+def prefixname_create(self, data, name):
+	nlen = len(name)
+	if nlen >= 0xFF:
+		raise FormatException("Filename too long")
+	return chr(nlen) + name + data
+def prefixname_extract(self, data):
+	nlen = ord(data[0])
+	name = data[1:nlen]
+	return (data[nlen:], name)
 
 class HiddenAlfa:
 	def __init__(self.image):
@@ -44,12 +47,7 @@ class HiddenAlfa:
 		if self.image.mode != 'RGBA':
 			raise ValueError("Image must be an RGBA image")
 		self.pixels = self.image.load()
-		self._zlib = ZlibFormat()
-		self._tar = TarFormat()
-		self.formats = {
-			"Z" : self._zlib,
-			"t" : self._tar
-		}
+		self.headlen = 5
 	
 	def usable_size(self):
 		size = 0
@@ -58,20 +56,18 @@ class HiddenAlfa:
 				if self.pixels[x, y][3] == 0:
 					size += 3
 		return size
-		
-	def _write_raw_data(self, data, flags=''):
-		headlen = 4 + len(flags) + 1
-		out = pack_size(len(data) + headlen) + flags + '|' + data
+
+	def _write_raw_data(self, data):
 		idx = 0
-		outlen = len(out)
-		out += '\x00' * (outlen % 3)
+		outlen = len(data)
+		data += '\x00' * (outlen % 3)
 		for x in self.image.size[0]:
 			for y in self.image.size[1]:
 				if self.pixels[x, y][3] == 0 and idx < outlen:
-					self.pixels[x, y] = (out[idx], out[idx + 1], out[idx + 2], 0)
+					self.pixels[x, y] = (data[idx], data[idx + 1], data[idx + 2], 0)
 					idx += 3
-		if idx < len(out):
-			raise IndexError("Could not write %i bytes of %i" % (len(out) - idx, len(out)))
+		if idx < len(data):
+			raise IndexError("Could not write %i bytes of %i" % (len(data) - idx, len(data)))
 	
 	def _read_raw_data(self):
 		data = []
@@ -79,44 +75,17 @@ class HiddenAlfa:
 			for y in self.image.size[1]:
 				if self.pixels[x, y][3] == 0:
 					data.extend(self.pixels[x, y][0:3])
-		data = ''.join(map(lamda x: chr(x), data))
-		datalen = unpack_size(data[0:4])
-		headbuff = data[4:13]
-		if  not '|' in headbuff:
-			raise ValueError("Header error")
-		flags = headbuff[0:headbuff.index('|')]
-		payloadstart = headbuff.index('|') + 4 + 1
+		return ''.join(map(lamda x: chr(x), data))
+		
+	def prefixlength_create(self, data, flags=0):
+		return struct.pack('<I', len(data) + self.headlen) + chr(flags) + data
+		
+	def prefixlength_extract(self, data, offset=0)
+		datalen = struct.unpack('<I', data[offset:offset+4])
+		flags = data[offset + self.headlen]
+		payloadstart = offset + self.headlen + 1
 		try:
 			payload = data[payloadstart:payloadstart+datalen]
 		except IndexError:
 			raise IndexError("Unexpected end of data")
 		return (payload, flags)
-	
-	def create(self, path='', fileobj=None, flags=''):
-		if os.path.isfile(path):
-			with open(path, 'rb') as f:
-				data = f.read()
-		elif fileobj:
-			data = fileobj.read()
-		elif os.path.isdir(path) and (not 't' in flags):
-			raise ValueError("Can't archive a directory without an archive format")
-		else:
-			raise IOError("File or directory not found: %s" % path)
-		for fl in flags:
-			data = self.formats[fl].create(path=path, data=data)
-		self._write_raw_data(data, flags=flags)
-
-	def extract(self, path='', fileobj=None):
-		if os.path.exists(path):
-			raise IOError("Destination alredy exists: %s" % path)
-		data, flags = _read_raw_data(self)
-		flags.reverse()
-		for fl in flags:
-			data = self.formats[fl].extract(path=path, data=data)
-		if data:
-			if fileobj:
-				fileobj.write(data)
-			else:
-				with open(path, 'wb') as f:
-					f.write(data)
-		
